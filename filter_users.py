51f163b142daae9f4ae6e4dddda3469a8c40dd8d
jugom <jugom@gmx.de>
2026-06-26 12:57:11 +0200

    Add filter-by-email function.

--- filter_users.py
+++ filter_users.py
@@ -4,11 +4,22 @@ with open("users.json", "r") as file:
     users = json.load(file)
 
 def filter_by_age(users):
-    filtered_users = []
+    filtered_users_by_age = []
     for user in users:
         if user["age"] > 31:
-            filtered_users.append(user)
-    return filtered_users
+            filtered_users_by_age.append(user)
+    return filtered_users_by_age
 
 for user in filter_by_age(users):
     print(user)
+
+
+def filter_by_email(users):
+    filtered_users_by_email = []
+    for user in users:
+        if "david" in user["email"]:
+            filtered_users_by_email.append(user)
+    return filtered_users_by_email
+
+print(filter_by_email(users))
+
